5e7aa2fb1f3d5356fcc510e7ceff136250d35e9e
vishal-codes <itsvishal2417@gmail.com>
2022-01-09 00:55:19 +0530

    made code shorter

--- src/util.js
+++ src/util.js
@@ -5,11 +5,7 @@ import { Circle, Popup } from "react-leaflet";
 export const sortData = (data) => {
     const sortedData = [...data];
     sortedData.sort((a,b) => {
-        if(a.cases > b.cases) {
-            return -1;
-        } else {
-            return 1;
-        }
+      return b.cases - a.cases;
     })
     return sortedData;
 }
